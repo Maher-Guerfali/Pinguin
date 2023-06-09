e552f9134f50c30e63a2a24e37187f165c0a2765
Maher Guerfali <47715285+Maher-Guerfali@users.noreply.github.com>
2023-06-09 05:04:34 +0200

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -56,7 +56,7 @@ export default () => {
         </button>
         <Link
             target="_blank"
-            href="http://pingoos.gg/Games"
+            href="http://games.pingoos.gg"
           >
             <div class="inline-block px-2 font-ubuntu h-10 bg-blue-400 rounded-lg cursor-pointer select-none active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841] active:border-b-[0px] transition-all duration-150 [box-shadow:2px_10px_1px_0_#1b6ff8,0_15px_0_0_#1b70f841] border-b-[1px] border-blue-400">
               <span class="flex flex-col justify-center items-center h-full text-white font-bold text-sm ">
