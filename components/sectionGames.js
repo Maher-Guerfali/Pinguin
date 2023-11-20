8ff65226e32901bd2245fdd701a6dc5a036a9ac1
Maher Guerfali <47715285+Maher-Guerfali@users.noreply.github.com>
2023-11-21 00:11:42 +0100

    Update sectionGames.js

--- components/sectionGames.js
+++ components/sectionGames.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 export default () => {
   const gameList = [
-    { img: "icey.png", nam: "Icey Bird", title: "icey" },
-    { img: "pinball.png", nam: "Pinball", title: "pinball" },
+    { img: "flying.png", nam: "Icey Bird", title: "icey", link: "https://www.google.com" },
+    { img: "pingoosball.png", nam: "Pinball", title: "pinball", link: "https://www.facebook.com" },
   ];
 
   return (
@@ -28,18 +28,20 @@ export default () => {
           src="/img/border-beta-bottom.png"
           alt=""
         />
-        <div className="max-w-screen-xl mx-auto grid grid-cols-2 my-10 gap-y-6">
+        <div className="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 my-10 gap-y-6">
           {gameList.map((item, index) => (
-            <div className="relative" key={index}>
-              <img
-                className="w-full"
-                src={`/games/${item.img}`}
-                alt=""
-              />
-              <h1 className="max-w-md mx-auto uppercase font-alpha text-blue text-center text-3xl">
-                {item.nam}
-              </h1>
-            </div>
+            <Link href={item.link} key={index}>
+              <a className="relative border border-gray-300 rounded p-4 transition duration-300 hover:border-blue hover:shadow-md">
+                <img
+                  className="w-full"
+                  src={`/games/${item.img}`}
+                  alt=""
+                />
+                <h1 className="max-w-md mx-auto uppercase font-alpha text-blue text-center text-3xl">
+                  {item.nam}
+                </h1>
+              </a>
+            </Link>
           ))}
         </div>
       </div>
