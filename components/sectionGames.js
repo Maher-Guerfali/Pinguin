feca55b070fe15ba4e9bdec12a8df2536aa012cc
Maher Guerfali <47715285+Maher-Guerfali@users.noreply.github.com>
2023-11-21 01:28:05 +0100

    Update sectionGames.js

--- components/sectionGames.js
+++ components/sectionGames.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default () => {
   const gameList = [
     { img: "playfly.png", nam: "Icey Bird", title: "icey", link: "https://pingoos.itch.io/icey-bird" },
-    { img: "playpin.png", nam: "Pinball", title: "pinball", link: "https://pingoos.itch.io/pingoos-pinball" },
+    { img: "playpin2.png", nam: "Pinball", title: "pinball", link: "https://pingoos.itch.io/pingoos-pinball" },
   ];
 
   return (
